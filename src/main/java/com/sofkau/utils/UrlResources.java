9af85e81d7405036178b1459365ef135ca58ba5d
juan david cardona velasquez <j.cardona1@utp.edu.co>
2023-04-12 23:12:50 -0500

    Se agregan los servicios rest de Listar Generacions de PokeApi y Agregar una nueva foto a un album

--- src/main/java/com/sofkau/utils/UrlResources.java
+++ src/main/java/com/sofkau/utils/UrlResources.java
@@ -6,7 +6,12 @@ public enum UrlResources {
     RESOURCES_PUT("users/"),
     SOAP_CAPITAL_BASE_URL("http://webservices.oorsprong.org/"),
     RESOURCE_CAPITAL("websamples.countryinfo/CountryInfoService.wso"),
-    BODY_PATH("src/test/resources/soapXml/filecapital.xml");
+    BODY_PATH("src/test/resources/soapXml/filecapital.xml"),
+
+    BASE_POKE_URL("https://pokeapi.co/"),
+    GENERATION1_RESOURCE("api/v2/generation/"),
+    PHOTO_POST_RESOURCE("/photos"),
+    JSONPLACEHOLDER_BASE_URL("https://jsonplaceholder.typicode.com");
 
     private final String value;
 
